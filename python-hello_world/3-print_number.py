8997c830694563e2eb23134b059d0c21e48f6cc1
m-mwangi <m.mwangi2@alustudent.com>
2023-07-07 03:48:26 -0700

    Task 3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for xup in range(ord('a'), ord('z') + 1):
-    if chr(xup) != 'q' and chr(xup) != 'e':
-        print("{}".format(chr(xup)), end='')
+number = 98
+print(f"{number:d} Battery street")
