3145a913a7d35c2112588db326ae6ce92943912b
m-mwangi <m.mwangi2@alustudent.com>
2023-08-02 08:25:10 +0200

    Task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,5 @@
--- Connect to MySQL server with appropriate credentials
-mysql -hlocalhost -uroot -p
-
--- List privileges for user_0d_1
+-- lists privileges of the MySQL users
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
--- Exit the MySQL client
-exit
 
