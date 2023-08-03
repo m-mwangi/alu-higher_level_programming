d310c446bcf82c89dd2f07eaf12f453aaa2cc2b9
m-mwangi <m.mwangi2@alustudent.com>
2023-08-03 13:43:41 -0700

    Task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
---  a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
--- Query to list all priviliges of users
+-- Lists all privileges of the users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
